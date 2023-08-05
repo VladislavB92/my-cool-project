2ac7eaf433c78c4d1c99014d54905bb931b85a63
vlad <vladislavs@codnity.com>
2023-08-05 12:06:36 +0300

    Change price field to CharField

--- my_cool_project/products/models.py
+++ my_cool_project/products/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(null=False, blank=False, max_length=128)
-    price = models.FloatField(null=False, blank=False)
+    price = models.CharField(null=False, blank=False, max_length=6)
